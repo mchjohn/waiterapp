651b5c465037e6465a5d283c70f07e89e005745f
Michel John <michel.john@hotmail.com>
2023-01-11 12:14:44 -0300

    config mongodb and mongoose and models

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
 
-const app = express();
+    console.log('✅ Connected to MongoDB');
 
-const PORT = 3001;
+    const PORT = 3002;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
-});
+    app.listen(PORT, () => {
+      console.log(`🚀 Server is running on http://localhost:${PORT}`);
+    });
+  }
+  )
+  .catch(() => console.log('❗Error to connect MongoDB'));
+
