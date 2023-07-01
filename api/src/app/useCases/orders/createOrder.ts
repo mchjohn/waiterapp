69eddb62a11c67aa82bb114f3756ae039ac405ff
Michel John <michel.john@hotmail.com>
2023-07-01 19:00:42 -0300

    add socket io on api

--- api/src/app/useCases/orders/createOrder.ts
+++ api/src/app/useCases/orders/createOrder.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 
+import { io } from '../../../index';
 import { Order } from '../../models/Order';
 
 export async function createOrder(req: Request, res: Response) {
@@ -10,6 +11,10 @@ export async function createOrder(req: Request, res: Response) {
       table,
       products,
     });
+
+    const orderDetails = await order.populate('products.product');
+
+    io.emit('orders@new', orderDetails);
 
     res.status(201).json(order);
   } catch (error) {
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,21 +1,32 @@
+import http from 'node:http';
 import path from 'node:path';
 import express from 'express';
 import mongoose from 'mongoose';
+import { Server } from 'socket.io';
 
 import { router } from './router';
+
+const app = express();
+const server = http.createServer(app);
+export const io = new Server(server);
 
 mongoose.connect('mongodb://localhost')
   .then(() => {
     console.log('✅ Connect to MongoDB');
-
-    const app = express();
     const PORT = 3001;
 
+    app.use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', '*');
+      res.setHeader('Access-Control-Allow-Headers', '*');
+
+      next();
+    });
     app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
     app.use(express.json());
     app.use(router);
 
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
       console.log(`🚀 Server is running at http://localhost:${PORT}`);
     });
   })
